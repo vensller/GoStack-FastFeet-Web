fe2ca9f6f56e6717debcaf56b260f4c4caf8c158
Ivens Diego Müller <ivens.muller@amazingit.com.br>
2020-04-15 19:10:29 -0300

    Recipients List done

--- src/pages/RecipientList/index.js
+++ src/pages/RecipientList/index.js
@@ -1,21 +1,88 @@
-import React from 'react';
-import { MdAdd } from 'react-icons/md';
+import React, { useState, useEffect } from 'react';
+import { MdAdd, MdSearch, MdMoreHoriz } from 'react-icons/md';
 
-import { Container } from './styles';
+import { Container, StyledTable } from './styles';
 import { StyledButton } from '~/global/styles';
+import Pagination from '~/components/Pagination';
 
 import history from '~/services/history';
+import api from '~/services/api';
 
 export default function RecipientList() {
+  const [page, setPage] = useState(1);
+  const [searchName, setSearchName] = useState('');
+  const [name, setName] = useState('');
+  const [recipients, setRecipients] = useState([]);
+
+  useEffect(() => {
+    async function loadRecipientes() {
+      const { data } = await api.get('/recipients', {
+        params: {
+          name: searchName,
+          page,
+          count: 10,
+        },
+      });
+      setRecipients(data);
+    }
+
+    loadRecipientes();
+  }, [page, searchName]);
+
+  function handleKeyUp(e) {
+    if (e.keyCode === 13) setSearchName(name);
+  }
+
   return (
     <Container>
-      <StyledButton
-        colored
-        onClick={() => history.push('/recipients/register')}
-      >
-        <MdAdd color="#fff" size={24} />
-        Cadastrar
-      </StyledButton>
+      <h2>Gerenciando destinatários</h2>
+      <div>
+        <div>
+          <MdSearch color="#999" size={24} />
+          <input
+            type="text"
+            placeholder="Busca por destinatários"
+            value={name}
+            onChange={e => setName(e.target.value)}
+            onKeyUp={handleKeyUp}
+          />
+        </div>
+        <StyledButton
+          colored
+          onClick={() => history.push('/recipients/register')}
+        >
+          <MdAdd color="#fff" size={24} />
+          Cadastrar
+        </StyledButton>
+      </div>
+
+      <StyledTable>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Endereço</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {recipients.map(item => (
+            <tr key={item.id}>
+              <td>{`#${item.id}`}</td>
+              <td>{item.name}</td>
+              <td>{item.address.full_address}</td>
+              <td>
+                <MdMoreHoriz color="#666" size={24} />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </StyledTable>
+      <Pagination
+        page={page}
+        onPageChange={increase => setPage(page + increase)}
+      />
     </Container>
   );
 }
--- src/pages/RecipientList/styles.js
+++ src/pages/RecipientList/styles.js
@@ -5,5 +5,98 @@ export const Container = styled.div`
   margin: 20px auto;
   display: flex;
   flex-direction: column;
-  padding: 10px;
+
+  div {
+    margin-top: 10px;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    flex-wrap: wrap;
+
+    div {
+      width: 350px;
+      display: flex;
+      flex-direction: row;
+      background: #fff;
+      border-radius: 4px;
+      align-content: center;
+      align-items: center;
+
+      svg {
+        margin-left: 5px;
+      }
+
+      input {
+        border: 0;
+        height: 32px;
+        width: 300px;
+        background: none;
+        padding: 0 5px;
+        color: #666;
+        max-width: 90%;
+
+        &::placeholder {
+          color: #999;
+        }
+      }
+    }
+
+    button {
+      margin-top: 10px;
+
+      @media (max-width: 680px) {
+        margin-left: 0px;
+      }
+    }
+  }
+`;
+
+export const StyledTable = styled.table`
+  margin-top: 20px;
+  border: none;
+  border-collapse: separate;
+  width: 100%;
+  border-spacing: 0 0.8em;
+
+  th,
+  td {
+    text-align: left;
+
+    &:last-child {
+      width: 50px;
+      max-width: 20%;
+      text-align: center;
+    }
+
+    &:first-child {
+      width: 50px;
+      text-align: left;
+      padding-left: 10px;
+    }
+  }
+
+  tbody {
+    tr {
+      background: #fff;
+      padding: 0;
+      margin: 10px 0;
+      border: 0;
+      height: 50px;
+
+      & td:first-child {
+        border-top-left-radius: 4px;
+      }
+      & td:last-child {
+        border-top-right-radius: 4px;
+      }
+      & td:first-child {
+        border-bottom-left-radius: 4px;
+      }
+      & td:last-child {
+        border-bottom-right-radius: 4px;
+      }
+    }
+  }
 `;
